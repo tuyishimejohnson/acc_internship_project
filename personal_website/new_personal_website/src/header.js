1cf59d713454318bc2b2cc478e4e31b8a6fe1c71
tuyishimejohnson <j.tuyishime4@alustudent.com>
2024-01-28 15:39:18 +0200

    Made some changes.

--- personal_website/new_personal_website/src/header.js
+++ personal_website/new_personal_website/src/header.js
@@ -29,20 +29,47 @@ const Header = () => {
           </div>
           <h2 className='text-center font-extrabold text-4xl my-8'>Profile</h2>
           <div className='flex justify-between'>
-            <img src={require('./1-intro-photo-final.jpg')} alt="" className='w-2/4 rounded-tl-3xl'/>
+            <img src={require('./1-intro-photo-final.jpg')} alt="" className='w-2/4 rounded-e-full'/>
             <div>
               <h3 className='text-center py-5 font-extrabold text-3xl'>About Me</h3>
               <p className='text-center px-6 text-3xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci fugiat, deserunt praesentium cupiditate necessitatibus eaque vitae iure, exercitationem libero doloribus quidem aperiam non. Quod animi eveniet, consequatur itaque provident deleniti!</p>
-              <div className='flex gap-4 items-center my-8 text-2xl justify-center'>
+              <div className='flex gap-4 items-center my-8 text-sm justify-center'>
                 < FontAwesomeIcon icon={faEnvelope} />
-                <p>email</p>
+                <p>sample@gmail.com</p>
                 < FontAwesomeIcon icon={faPhone} />
-                <p>phone</p>
+                <p>+25076888888</p>
                 < FontAwesomeIcon icon={faMapMarker} />
-                <p>location</p>
+                <p>Kg 38 St Kigali, Rwanda</p>
               </div>
             </div>
           </div>
+          <h2 className='font-extrabold text-3xl w-2/4 text-center my-9'>Contact me</h2>
+          <footer className='bg-slate-800 px-7'>
+            <form action="">
+              <fieldset>
+                <div className='w-2/4'>
+                  <div className='block mb-2'>
+                    <label htmlFor="name">Names</label>
+                    <input type="text" id='name' placeholder='Your names' className='w-full text-gray-600'/>
+                  </div>
+                  
+                  <div className='block mb-2'>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id='email' placeholder='Your email' className='w-full text-gray-600'/>
+                  </div>
+                  
+                  <div className='block mb-2'>
+                    <label htmlFor="message">Your Message</label>
+                    <textarea name="message" id="message" cols="30" rows="10" placeholder='Your message' className='w-full text-gray-600'></textarea>
+                  </div>
+                  <button type="submit" className='px-4 py-3 rounded-sm border hover:bg-white text-black'>Send message</button>
+                </div>
+              </fieldset>
+            </form>
+            <div>
+                
+            </div>
+          </footer>
         </div>
           
     </>
